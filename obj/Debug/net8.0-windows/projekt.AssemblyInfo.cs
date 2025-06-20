5362603e0b6f8867c0f43b43bc24f5e1e4904d8e
spnbeatz <spnbeatz@gmail.com>
2025-06-20 21:23:06 +0200

    Zapomnialem dodac sql

--- obj/Debug/net8.0-windows/projekt.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/projekt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8fd233fe09405a7aac0f867b746f9b8b288da9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
